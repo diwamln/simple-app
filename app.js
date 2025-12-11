6752d4e1c77b5843f52c81bdcd0ef4371c72ddcf
dwcfree <129719836+diwamln@users.noreply.github.com>
2025-12-11 11:27:14 +0700

    Fix typo in express.json middleware usage

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Middleware
-app.use(express.jsonn());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
