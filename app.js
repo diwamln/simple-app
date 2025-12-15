7c3da065f6378b0e49be3cd643559859a8ff36b1
Egiiiiii <titanegif@gmail.com>
2025-12-15 13:08:40 +0700

    test loki

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-// app.js
 const express = require('express');
 const app = express();
+
+/**
+ * 🔥 REQUEST LOGGING (HARUS PALING ATAS)
+ */
+app.use((req, res, next) => {
+  console.log(JSON.stringify({
+    service: "simple-app",
+    method: req.method,
+    path: req.originalUrl,
+    ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+    user_agent: req.headers['user-agent'],
+    time: new Date().toISOString()
+  }))
+  next()
+})
 
 // Middleware
 app.use(express.json());
@@ -8,35 +22,29 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('apps versi 3, deploy success jir'); // Konten bebas diganti
+  res.send('adili jokowi');
 });
 
 app.post('/test', (req, res) => {
-    res.json({
-        message: 'Data received',
-        yourData: req.body
-    });
+  res.json({
+    message: 'Data received',
+    yourData: req.body
+  });
 });
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-
 // 404 handler
 app.use((req, res) => {
-    res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: 'Route not found' });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Internal server error' });
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
 });
 
-
-
-
-
-// Export app tanpa listen (untuk testing)
 module.exports = app;
